07c645dd366c43b5f7cf9f895b52e82f07f718d3
Tempire <danil.varlamov.03@mail.ru>
2023-07-08 12:44:59 +0300

    add questions controller and config routes  questions  for PATCH request

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,20 @@
 class QuestionsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
+
     Question.create(
-    body: params[:question][:body],
-    user_id: params[:question][:user_id]
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+      )
+
+  end
+
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
   post '/questions', to: 'questions#create'
+  patch 'questions/:id', to: 'questions#update'
 end
