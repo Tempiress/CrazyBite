98e2d9d28915264100f956df5af4c24a233812e9
Tempire <danil.varlamov.03@mail.ru>
2023-07-11 18:32:02 +0300

    configure applications controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,5 +28,20 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def main
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/',to: 'questions#main'
   post '/questions', to: 'questions#create'
-  patch 'questions/:id', to: 'questions#update'
+  patch 'questions/:id', to: 'questions#update', as: 'question'
   delete 'questions/:id', to: 'questions#destroy'
+  get '/questions/:id/edit', to: 'questions#edit'
+  get '/questions/new', to: 'questions#new'
   get 'questions/:id', to: 'questions#show'
+  get '/questions', to: 'questions#index'
+
 end
